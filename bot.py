94ea072076cb355b2acb28f4c74ea46842912075
iSabbir <131774907+iSabbir@users.noreply.github.com>
2023-04-30 22:07:25 +0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -6,7 +6,7 @@ from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 telegram_token = os.environ.get('TELEGRAM_TOKEN')
 
 # Get the Mail.tm API key from the environment variable
-mail_api_key = os.environ.get('MAIL_API_KEY')
+mail_api_key = os.environ.get('eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJpYXQiOjE2ODI4NzA2MTksInJvbGVzIjpbIlJPTEVfVVNFUiJdLCJ1c2VybmFtZSI6InNpaHNia2R0dG96ekBidWdmb28uY29tIiwiaWQiOiI2NDMzMTM5MGM5OWY5NGMwMGIzODA0ODUiLCJtZXJjdXJlIjp7InN1YnNjcmliZSI6WyIvYWNjb3VudHMvNjQzMzEzOTBjOTlmOTRjMDBiMzgwNDg1Il19fQ.hesHvDBUN-LHrELIWNXlwlfxDrXddUZOBIWawPpvGCJ0JM_514LNdCXsbyYQT23hEhmqt2nOkE-kxhGaYxUcDQ')
 
 def start(update, context):
     context.bot.send_message(chat_id=update.effective_chat.id, text="Welcome to my temp mail bot! Send me a message and I'll forward it to your temporary email address.")
